c30067d3b583f39f025fa14dc783d2ab4af0ca53
Kasper Sauramo <kasper.sauramo@cs.helsinki.fi>
2015-02-19 10:33:30 +0200

    Added name field

--- src/ks_objprs.cpp
+++ src/ks_objprs.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 
 bool32
-parseObj( const char *filename, Geometry *g)
+parseObj( const char *filename, Mesh *m)
 {
     //TODO(kasper): Buffer overflow
     char buffer[32];
@@ -19,20 +19,26 @@ parseObj( const char *filename, Geometry *g)
         if( strcmp( buffer, "v" ) == 0) {
             glm::vec3 v;
             fscanf( file, "%f %f %f\n", &v.x, &v.y, &v.z);
-            g->vertices.push_back( v);
+            m->vertices.push_back( v);
 
         } else if( strcmp( buffer, "vn") == 0) {
             glm::vec3 vn;
             fscanf( file, "%f %f %f\n", &vn.x, &vn.y, &vn.z);
-            g->vertexNormals.push_back( vn);
+            m->vertexNormals.push_back( vn);
 
         } else if( strcmp( buffer, "f") == 0) {
             uint32 i1, i2, i3;
             fscanf( file, "%d//%*d %d//%*d %d//%*d\n", 
                     &i1, &i2, &i3);
-            g->faceVertexIndices.push_back(i1);
-            g->faceVertexIndices.push_back(i2);
-            g->faceVertexIndices.push_back(i3);
+            m->faceVertexIndices.push_back(i1);
+            m->faceVertexIndices.push_back(i2);
+            m->faceVertexIndices.push_back(i3);
+
+        } else if( strcmp( buffer, "o") == 0) {
+            //TODO(kasper): Buffer overflow
+            char name[128];
+            fscanf( file, "%s\n", name);
+            strncpy( m->name, name, strlen( name));
         }
     }
 
